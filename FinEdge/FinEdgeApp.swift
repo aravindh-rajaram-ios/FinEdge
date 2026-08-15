ba0b7272dbab5c1e73d049c9ea3462b58b4f9ed9
Aravindh Rajaram <aravindhrajaram@Aravindhs-MacBook-Air.local>
2026-08-15 20:23:04 +0530

    Initial FinEdge iOS Project

---
+++ FinEdge/FinEdgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinEdgeApp.swift
+//  FinEdge
+//
+//  Created by Aravindh Rajaram on 15/08/26.
+//
+
+import SwiftUI
+
+@main
+struct FinEdgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FinEdgeTests/FinEdgeTests.swift
@@ -0,0 +1,19 @@
+//
+//  FinEdgeTests.swift
+//  FinEdgeTests
+//
+//  Created by Aravindh Rajaram on 15/08/26.
+//
+
+import Testing
+@testable import FinEdge
+
+struct FinEdgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
